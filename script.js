e21b9abb6f365944651a738f1eed6fc1c4c16308
Luca Ruben <luk.ruben9@yahoo.com>
2024-09-26 19:02:26 +0200

    modificări efectuate

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-var myHeaders = new Headers();
-myHeaders.append("x-rapidapi-key", "1d8e2196aea2407e5d9353fd2812d237");
-myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+const API_KEY = "1d8e2196aea2407e5d9353fd2812d237";
+const BASE_URL = "https://v3.football.api-sports.io/";
 
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
+async function fetchData(endPoint) {
+  try {
+    const apiURL = BASE_URL + endPoint;
+    const response = await fetch(apiURL, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "1d8e2196aea2407e5d9353fd2812d237",
+        "x-rapidapi-host": "v3.football.api-sports.io",
+      },
+      redirect: "follow",
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-fetch("https://v3.football.api-sports.io/leagues", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
- 
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const widget = document.getElementById('wg-api-football-games');
+  const dateInput = document.getElementById('data-date');
+
+  const today = new Date().toISOString().split('T')[0];
+  widget.setAttribute('data-date', today); 
+
+
+  
+});
